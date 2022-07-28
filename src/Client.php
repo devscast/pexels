ee81ae12a9fa5192d4ed38974ab5a27d2a41731b
bernard-ng <ngandubernard@gmail.com>
2022-07-28 12:15:19 +0200

    fix(style): remove unused Collection class

--- src/Client.php
+++ src/Client.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Devscast\Pexels;
 
-use Devscast\Pexels\Data\Collection;
 use Devscast\Pexels\Data\CollectionMedia;
 use Devscast\Pexels\Data\Collections;
 use Devscast\Pexels\Data\Photo;
